e8570eeb1615e443699fb743ee3506e3db82b03b
yasieldev <contactoyasiel@gmail.com>
2024-10-17 00:37:44 -0500

    techlog_project1.5

--- back/DB/consults.js
+++ back/DB/consults.js
@@ -1,6 +1,6 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");  // Cambiado a mysql2
 const services = require(`${__dirname}/../Auth/authservices.js`);
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT} = require("../../config.js");
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = require("../../config.js");
 
 let connection = mysql.createConnection({
   host: DB_HOST,
@@ -8,7 +8,10 @@ let connection = mysql.createConnection({
   user: DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
-})
+  authPlugins: {
+    caching_sha2_password: () => mysql.authPlugins.caching_sha2_password
+  }
+});
 
 connection.connect(function (err) {
   if (err) {
@@ -16,62 +19,49 @@ connection.connect(function (err) {
   } else {
     console.log("connected");
   }
-})
-
+});
 
 function createUser(username, gmail, password) {
-
   return new Promise((resolve, reject) => {
-
-
     verifyUser(username).then(verify => {
       if (verify) {
-
         addUser(username, gmail, password)
           .then(message => {
-            console.log(message)
-            //connection.end();
+            console.log(message);
             resolve(message);
           })
           .catch(err => {
             console.log(err);
-            // connection.end();
             reject(err);
           });
-
       } else {
-        console.log("el usuario ya existe");
-        //connection.end();
-        reject("el usuario ya existe");
-
+        console.log("El usuario ya existe");
+        reject("El usuario ya existe");
       }
     }).catch(e => {
-      console.log(`ha ocurrido un error en la verificacion del usuario: ${e}`);
+      console.log(`Ha ocurrido un error en la verificación del usuario: ${e}`);
       return false;
-      //connection.end();
-    })
-  })
-
-
+    });
+  });
 }
 
 function verifyUser(username) {
   return new Promise((resolve, reject) => {
-    connection.query(`SELECT * FROM users WHERE username LIKE ?`, username, (err, res, fields) => {
+    connection.query(`SELECT * FROM users WHERE username LIKE ?`, [username], (err, res, fields) => {
       if (err) {
         reject(err);
       } else {
         resolve(res.length === 0);
       }
-    })
-  })
+    });
+  });
 }
 
 function addUser(username, gmail, password) {
   return new Promise((resolve, reject) => {
-    connection.query(`INSERT INTO users (username, gmail, password) VALUES (?,?,?)`, [username, gmail, password], (err, res, fields) => {
+    connection.query(`INSERT INTO users (username, gmail, password) VALUES (?, ?, ?)`, [username, gmail, password], (err, res, fields) => {
       if (err) {
-        reject("ha ocurrido un error:" + err);
+        reject("Ha ocurrido un error: " + err);
       } else {
         resolve("El usuario ha sido creado");
       }
@@ -80,36 +70,38 @@ function addUser(username, gmail, password) {
 }
 
 function login(username, password) {
-  return new Promise((resolve, reject)=>{
-    connection.query(`SELECT * FROM users WHERE username LIKE ?`, username, (err, res, fields)=>{
-      if(err){
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT * FROM users WHERE username LIKE ?`, [username], (err, res, fields) => {
+      if (err) {
         reject(err);
-      } else if (res.length === 0){
-        reject("el usuario no existe, debe registrarse para poder iniciar sesion")
+      } else if (res.length === 0) {
+        reject("El usuario no existe, debe registrarse para poder iniciar sesión");
       } else {
-        comparePassword(username, password).then(res=>resolve(res)).catch(e=>reject(e))
+        comparePassword(username, password).then(res => resolve(res)).catch(e => reject(e));
       }
-    })
-})
+    });
+  });
 }
 
-function comparePassword(username, password){
-  return new Promise((resolve, reject)=>{
-    connection.query(`SELECT password FROM users WHERE username = ?`, username, (err, res, field)=>{
-      if(err){
-        reject(err)
-      }
-        services.verifyPassword(password, res[0].password).then((res)=>{
-          if(res){
-            resolve(`hola ${username}, ha iniciado sesion correctamente`);
+function comparePassword(username, password) {
+  return new Promise((resolve, reject) => {
+    connection.query(`SELECT password FROM users WHERE username = ?`, [username], (err, res, field) => {
+      if (err) {
+        reject(err);
+      } else {
+        services.verifyPassword(password, res[0].password).then((res) => {
+          if (res) {
+            resolve(`Hola ${username}, ha iniciado sesión correctamente`);
           } else {
-            reject("la clave es incorrecta");
+            reject("La clave es incorrecta");
           }
-        }).catch((e)=>{
+        }).catch((e) => {
           reject(e);
-        })
-    })  
-  })
+        });
+      }
+    });
+  });
 }
 
-module.exports = { createUser, login};
+module.exports = { createUser, login };
+
